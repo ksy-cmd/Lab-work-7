b20c3e1e8bea07ba2c8fdb2612fa3f19880053ac
ksy-cmd <krastikrabs200@gmail.com>
2026-05-15 18:26:04 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Lab-work-7/',  // ← заміни на назву твого репозиторію
+  base: './',  // ← ЗМІНА: './' замість '/Lab-work-7/'
 })
