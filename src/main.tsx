0537ecbc3b406813f5a4a612dd35791520fb7460
ksy-cmd <krastikrabs200@gmail.com>
2026-05-15 18:35:04 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'  // ← ЗМІНА: HashRouter замість BrowserRouter
+import { HashRouter } from 'react-router-dom' // <-- Імпортуємо HashRouter
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-        <HashRouter>  {/* ← ЗМІНА */}
-            <App />
-        </HashRouter>
-    </React.StrictMode>
+  <React.StrictMode>
+    <HashRouter> {/* <-- Замінюємо BrowserRouter на HashRouter */}
+      <App />
+    </HashRouter>
+  </React.StrictMode>,
 )
